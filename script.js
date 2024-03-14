71d73d89fd16d79a246ac0b3ed94e5b4c03ce0e7
lalitpatel11 <patellalit626@gmail.com>
2024-03-14 23:03:31 +0530

    Ui changes

--- script.js
+++ script.js
@@ -6,3 +6,55 @@ progressBars.forEach((progressBar) => {
   const progress = progressBar.dataset.progress;
   progressBar.style.setProperty("--progress-width", progress);
 });
+
+const words = [
+  "React Native Dev",
+  "Node JS Dev",
+  "Flutter Dev",
+  "UI/UX Designer",
+];
+
+const colors = ["blue", "green", "yellow", "red"];
+text = document.querySelector(".text");
+
+function* generator() {
+  var index = 0;
+  while (true) {
+    yield index++;
+
+    if (index > 3) {
+      index = 0;
+    }
+  }
+}
+
+function printChar(word) {
+  let i = 0;
+  text.innerHTML = "";
+  text.classList.add(colors[words.indexOf(word)]);
+  let id = setInterval(() => {
+    if (i >= word.length) {
+      deleteChar();
+      clearInterval(id);
+    } else {
+      text.innerHTML += word[i];
+      i++;
+    }
+  }, 500);
+}
+function deleteChar() {
+  let word = text.innerHTML;
+  let i = word.length - 1;
+  let id = setInterval(() => {
+    if (i >= 0) {
+      text.innerHTML = text.innerHTML.substring(0, text.innerHTML.length - 1);
+      i--;
+    } else {
+      text.classList.remove(colors[words.indexOf(word)]);
+      printChar(words[gen.next().value]);
+      clearInterval(id);
+    }
+  }, 300);
+}
+let gen = generator();
+printChar(words[gen.next().value]);
